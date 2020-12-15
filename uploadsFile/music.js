74bb6dc4803037d4202110b7fb17d3897fc5a8e8
btKrc1nx <33425848@qq.com>
2020-12-15 23:59:17 +0800

    update

--- uploadsFile/music.js
+++ uploadsFile/music.js
@@ -145,11 +145,6 @@ var ap = new APlayer({
     url: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource2@main/music/%E7%AB%8B%E5%86%AC.m4a',
     pic: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource@main/20201130184505.png',
   }, {
-    title: 'DEAR JOHN',
-    author: '比莉',
-    url: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource2@main/music/DEAR%20JOHN.m4a',
-    pic: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource@main/20201130184505.png',
-  }, {
     title: '癒し イヤラシ☆これって何ナニ',
     author: '伊ヶ崎綾香',
     url: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource2@main/music/%E7%99%92%E3%81%97%20%E3%82%A4%E3%83%A4%E3%83%A9%E3%82%B7%E2%98%86%E3%81%93%E3%82%8C%E3%81%A3%E3%81%A6%E4%BD%95%E3%83%8A%E3%83%8B.mp3',
@@ -173,6 +168,11 @@ var ap = new APlayer({
     title: '今夜的风',
     author: '程响',
     url: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource2@main/music/%E4%BB%8A%E5%A4%9C%E7%9A%84%E9%A3%8E.m4a',
+    pic: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource@main/20201130184505.png',
+  }, {
+    title: 'Deep Blue Townへおいでよ',
+    author: '内田真礼',
+    url: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource2@main/music/Deep%20Blue%20Town%E3%81%B8%E3%81%8A%E3%81%84%E3%81%A7%E3%82%88%20.m4a',
     pic: 'https://cdn.jsdelivr.net/gh/btKrc1nx/resource@main/20201130184505.png',
   },]
 });

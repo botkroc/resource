048ce3ed2468c2f2fac01d580acdf0e228a6ad18
btKrc1nx <33425848@qq.com>
2021-02-22 14:51:38 +0800

    update

--- uploadsFile/myUtils.js
+++ uploadsFile/myUtils.js
@@ -1,2 +1,3 @@
-document.getElementsByClassName("site-page")[1].target = "_self";
+document.getElementById("sidebar-menus").getElementsByClassName("site-page")[1].target = "_self";
 document.getElementById("card-info-btn").target = "_self";
+document.getElementById("menus").getElementsByClassName("site-page")[1].target = "_self";

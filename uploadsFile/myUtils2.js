29f1ed7d664a6eeb8b2afd367ca87da372e4a5e2
btKrc1nx <33425848@qq.com>
2021-03-05 16:46:12 +0800

    update

--- uploadsFile/myUtils2.js
+++ uploadsFile/myUtils2.js
@@ -1,3 +1,7 @@
 document.getElementById("sidebar-menus").getElementsByClassName("site-page")[1].target = "_self";
-document.getElementById("card-info-btn").target = "_self";
+var $cardInfoBtn = document.getElementById("card-info-btn");
+if ($cardInfoBtn) {
+    /**有的页面可能不存在此widget */
+    $cardInfoBtn.target = "_self";
+}
 document.getElementById("menus").getElementsByClassName("site-page")[1].target = "_self";

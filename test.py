074f11a0132009af107de8d1d2cfe769c0a9463c
btKrc1nx <33425848@qq.com>
2021-06-23 08:31:13 +0800

    script upload

---
+++ test.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+import os
+import pyperclip
+import shutil
+
+
+# 如果为真则复制markdown格式的url，反之则是普通格式
+url_markdown = True
+
+repoPath = "F:\\C_program\\PracticeWeb\\issueRoot\\resource\\img\\"
+customUrl = "https://cdn.jsdelivr.net/gh/btkrc/resource@main/img/"
+
+imgPath = sys.argv[1]
+imgFileName = imgPath.split("\\", -1)[-1]
+
+shutil.move(imgPath, repoPath)
+
+data = os.popen("F:\\C_program\\PracticeWeb\\issueRoot\\resource\\push.sh")
+print(data.read())
+
+# os.system("F:\\C_program\\PracticeWeb\\issueRoot\\resource\\push.sh")
+
+if(url_markdown):
+    pyperclip.copy("![]("+customUrl+imgFileName+")")
+else:
+    pyperclip.copy(customUrl+imgFileName)
+
+os.system("pause")

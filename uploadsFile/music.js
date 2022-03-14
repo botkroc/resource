4d3d569c1676a3f58bdbe50ce14c35b6bf3bc20d
btKrc1nx <33425848@qq.com>
2022-03-14 16:30:20 +0800

    upload

--- uploadsFile/music.js
+++ uploadsFile/music.js
@@ -17,92 +17,112 @@ var ap = new APlayer({
     author: '鏡音リン/みきとP',
     url: 'http://music.163.com/song/media/outer/url?id=546724682',
     pic: 'http://p2.music.126.net/scCoTzSWV14xl_9ZGecCQA==/109951164192574921.jpg?param=130y130',
-  },{
+  }, {
     title: 'えれくとりっく',
     author: '初音ミク/ヤスオP',
     url: 'http://music.163.com/song/media/outer/url?id=22677533',
     pic: 'http://p2.music.126.net/byXQIhAT-iS6xd6icx8EiA==/560750930187412.jpg?param=130y130',
-  },{
+  }, {
     title: '热爱105°C的你',
     author: '早稻叽',
     url: 'https://cdn.jsdelivr.net/gh/btkrc/resource@main/bgm/%E7%83%AD%E7%88%B1105%C2%B0C%E7%9A%84%E4%BD%A0-%E6%97%A9%E7%A8%BB%E5%8F%BD.mp3',
     pic: 'https://cdn.jsdelivr.net/gh/btkrc/resource@main/bgm/109951166087945459.jpg',
-  },{
+  }, {
     title: 'Conqueror',
     author: 'IA',
     url: 'https://cdn.jsdelivr.net/gh/btkrc/resource@main/bgm/Conqueror-IA.m4a',
     pic: 'https://cdn.jsdelivr.net/gh/btkrc/resource@main/bgm/109951163244804271.jpg',
-  },{
+  }, {
+    title: 'Lifestyle',
+    author: 'MAGES',
+    url: 'http://music.163.com/song/media/outer/url?id=1420745339',
+    pic: 'http://p1.music.126.net/UK8H6_DIXFye2KLW-j7ExA==/109951165230280769.jpg?param=130y130',
+  }, {
     title: 'トルコ行進曲',
     author: '96猫',
     url: 'http://music.163.com/song/media/outer/url?id=28403160',
     pic: 'http://p1.music.126.net/vrOG0hAhslI4djtZdMgE6g==/6044015418094303.jpg?param=130y130',
-  },{
+  }, {
     title: 'ちるちる',
     author: 'REOL',
     url: 'http://music.163.com/song/media/outer/url?id=436016476',
     pic: 'http://p2.music.126.net/cu1sEIDxXOJm5huZ3Wjs0Q==/18833534672880379.jpg?param=130y130',
-  },{
+  }, {
     title: 'アシンメトリー',
     author: 'REOL',
     url: 'http://music.163.com/song/media/outer/url?id=28941716',
     pic: 'http://p1.music.126.net/cZPx3peGTuWEI_GaZB5CDg==/8892850045794893.jpg?param=130y130',
-  },{
+  }, {
     title: 'ふわふわ♪',
     author: '樹莓蛋奶酥',
     url: 'http://music.163.com/song/media/outer/url?id=440767322',
     pic: 'http://p1.music.126.net/9Tp9wWekfaBSimmba6ftLA==/109951165599339491.jpg?param=130y130',
-  },{
+  }, {
+    title: '让风告诉你 (日文版)',
+    author: 'Hanser / 泠鸢yousa',
+    url: 'http://music.163.com/song/media/outer/url?id=1831504253',
+    pic: 'http://p2.music.126.net/pfnSmGMFMNcsCMOua6DMTw==/109951165825771311.jpg?param=130y130',
+  }, {
+    title: '【取经计划·原创】新·九九八十一（官方优化版）',
+    author: '泠鸢yousa',
+    url: 'http://music.163.com/song/media/outer/url?id=498942717',
+    pic: 'http://p2.music.126.net/k8Fxa118GP34K0VTGKWaDg==/109951163003648209.jpg?param=130y130',
+  }, {
+    title: '怪物',
+    author: '多多poi / Hanser',
+    url: 'http://music.163.com/song/media/outer/url?id=1854902495',
+    pic: 'http://p1.music.126.net/GDxBDUX9OGoyxe2KRiV93A==/109951166105612973.jpg?param=130y130',
+  }, {
     title: '恋は渾沌の隷也',
     author: '後ろから這いより隊G',
     url: 'http://music.163.com/song/media/outer/url?id=31967343',
     pic: 'http://p2.music.126.net/IAMMhbg3RZ0boK9j3LQFfQ==/18788454697228143.jpg?param=130y130',
-  },{
+  }, {
     title: '「交织together」',
     author: 'DMYoung / Hanser / 泠鸢yousa',
     url: 'http://music.163.com/song/media/outer/url?id=460478768',
     pic: 'http://p1.music.126.net/JBo0QEBpHlzUNpTd5P4zuw==/109951162861954189.jpg?param=130y130',
-  },{
+  }, {
     title: 'カサネテク',
     author: 'Sakanya',
     url: 'http://music.163.com/song/media/outer/url?id=1490050203',
     pic: 'http://p2.music.126.net/jH2eR2gKvW5oDCe56UPVhA==/109951165415325309.jpg?param=130y130',
-  },{
+  }, {
     title: '深海少女',
     author: '初音ミク',
     url: 'http://music.163.com/song/media/outer/url?id=31140699',
     pic: 'http://p2.music.126.net/8jr6ElaxvntFk8LkKM4j2A==/7880199836870994.jpg?param=130y130',
-  },{
+  }, {
     title: '花月成双',
     author: '泠鸢yousa / 朽木素 / 魂喵喵',
     url: 'http://music.163.com/song/media/outer/url?id=1838510557',
     pic: 'http://p1.music.126.net/LhYYzZT-V_n4eNDNsOu0GQ==/109951165696869592.jpg?param=130y130',
-  },{
+  }, {
     title: '永夜のパレード',
     author: 'ケーキ姫☆優海 / JUMA',
     url: 'http://music.163.com/song/media/outer/url?id=692193',
     pic: 'http://p1.music.126.net/_0EPPAiKLQqddTZ5eKSqBw==/109951163136011717.jpg?param=130y130',
-  },{
+  }, {
     title: 'tutu',
     author: '鎖那',
     url: 'http://music.163.com/song/media/outer/url?id=1854628391',
     pic: 'http://p2.music.126.net/vQQham0ok6MLeY2GRIQNLA==/109951166101594087.jpg?param=130y130',
-  },{
+  }, {
     title: '约会宣言',
     author: 'iKz / Hanser',
     url: 'http://music.163.com/song/media/outer/url?id=1358213099',
     pic: 'http://p2.music.126.net/25XBZMlTz8bZBq6N54ei4g==/109951163891882131.jpg?param=130y130',
-  },{
+  }, {
     title: '勾指起誓',
     author: '泠鸢yousa',
     url: 'http://music.163.com/song/media/outer/url?id=1350160463',
     pic: 'http://p1.music.126.net/fxI_iAsaRU9M5ZGE-Pu7iw==/109951164479032071.jpg?param=130y130',
-  },{
+  }, {
     title: '【洛天依】影子小姐',
     author: '著小生zoki / 洛天依',
     url: 'http://music.163.com/song/media/outer/url?id=1298762936',
     pic: 'http://p1.music.126.net/xJmb740nKXk8paKRHg7Yqg==/109951163440366031.jpg?param=130y130',
-  },{
+  }, {
     title: '化身星海',
     author: '中国绊爱',
     url: 'http://music.163.com/song/media/outer/url?id=1458786291',
